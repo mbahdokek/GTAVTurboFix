0a2afbfcc97f5639801d12624bc4fb3e7085ccdb
E66666666 <emily66666666ok@outlook.com>
2020-04-08 19:17:51 +0200

    Move function body into objects

--- TurboFix/ScriptMenu.cpp
+++ TurboFix/ScriptMenu.cpp
@@ -3,141 +3,134 @@
 #include "Util/UI.hpp"
 
 #include <fmt/format.h>
-#include <inc/natives.h>
 
 CScriptMenu::CScriptMenu(std::string settingsFile,
                          std::function<void()> onInit,
                          std::function<void()> onExit )
     : mSettingsFile(std::move(settingsFile)) {
-    mMenu.RegisterOnMain(std::move(onInit));
-    mMenu.RegisterOnExit(std::move(onExit));
-    mMenu.SetFiles(mSettingsFile);
-    mMenu.Initialize();
-    mMenu.ReadSettings();
-}
+    mMenuBase.RegisterOnMain(std::move(onInit));
+    mMenuBase.RegisterOnExit(std::move(onExit));
+    mMenuBase.SetFiles(mSettingsFile);
+    mMenuBase.Initialize();
+    mMenuBase.ReadSettings();
 
-// This is ugly, but it'll have to do for now.
+    /* mainmenu */
+    mSubmenus.emplace_back(mMenuBase, "mainmenu", [](NativeMenu::Menu& mbCtx, CTurboScript& context) {
+        mbCtx.Title("Turbo Fix");
+        mbCtx.Subtitle(std::string("~b~") + Constants::DisplayVersion);
 
-void update_mainmenu(NativeMenu::Menu& menu, CTurboScript& context) {
-    menu.Title("Turbo Fix");
-    menu.Subtitle(std::string("~b~") + Constants::DisplayVersion);
+        mbCtx.BoolOption("Enable", context.Settings().Main.Enable,
+            { "Enable or disable the entire script." });
 
-    menu.BoolOption("Enable", context.Settings().Main.Enable,
-        { "Enable or disable the entire script." });
+        mbCtx.MenuOption("Configs", "configsmenu",
+            { "An overview of configurations available." });
 
-    menu.MenuOption("Configs", "configsmenu",
-        { "An overview of configurations available." });
+        CConfig* activeConfig = context.ActiveConfig();
+        mbCtx.MenuOption(fmt::format("Active config: {}", activeConfig ? activeConfig->Name : "None"),
+            "editconfigmenu",
+            { "Enter to edit the current configuration." });
+    });
 
-    CConfig* activeConfig = context.ActiveConfig();
-    menu.MenuOption(fmt::format("Active config: {}", activeConfig ? activeConfig->Name : "None"),
-        "editconfigmenu", 
-        { "Enter to edit the current configuration." });
-}
+    /* mainmenu -> configsmenu */
+    mSubmenus.emplace_back(mMenuBase, "configsmenu", [](NativeMenu::Menu& mbCtx, CTurboScript& context) {
+        mbCtx.Title("Configs");
+        mbCtx.Subtitle("Overview");
 
-void update_configsmenu(NativeMenu::Menu& menu, CTurboScript& context) {
-    menu.Title("Configs");
-    menu.Subtitle("Overview");
-
-    if (menu.Option("Reload configs")) {
-        context.LoadConfigs();
-        context.UpdateActiveConfig();
-    }
-
-    for (const auto& config : context.Configs()) {
-        bool selected;
-        menu.OptionPlus(config.Name, {}, &selected);
-
-        if (selected) {
-            std::vector<std::string> extras{
-                fmt::format("Models: {}", fmt::join(config.ModelNames, ", ")),
-                fmt::format("Plates: {}", fmt::join(config.Plates, ", ")),
-
-                fmt::format("RPM Spool Start: {:.2f}", config.RPMSpoolStart),
-                fmt::format("RPM Spool End: {:.2f}", config.RPMSpoolEnd),
-                fmt::format("Min boost: {:.2f}", config.MinBoost),
-                fmt::format("Max boost: {:.2f}", config.MaxBoost),
-                fmt::format("Spool rate: {:.5f}", config.SpoolRate),
-                fmt::format("Unspool rate: {:.5f}", config.UnspoolRate),
-
-            };
-
-            menu.OptionPlusPlus(extras);
+        if (mbCtx.Option("Reload configs")) {
+            context.LoadConfigs();
+            context.UpdateActiveConfig();
         }
-    }
-}
 
-void update_editconfigmenu(NativeMenu::Menu& menu, CTurboScript& context) {
-    menu.Title("Config edit");
-    CConfig* config = context.ActiveConfig();
-    menu.Subtitle(config ? config->Name : "None");
+        for (const auto& config : context.Configs()) {
+            bool selected;
+            mbCtx.OptionPlus(config.Name, {}, &selected);
 
-    if (config == nullptr) {
-        menu.Option("No active configuration");
-        return;
-    }
+            if (selected) {
+                std::vector<std::string> extras{
+                    fmt::format("Models: {}", fmt::join(config.ModelNames, ", ")),
+                    fmt::format("Plates: {}", fmt::join(config.Plates, ", ")),
 
-    menu.FloatOption("RPM Spool Start", config->RPMSpoolStart, 0.0f, 1.0f, 0.01f,
-        { "At what RPM the turbo starts spooling up.",
-          "0.2 RPM is idle."});
+                    fmt::format("RPM Spool Start: {:.2f}", config.RPMSpoolStart),
+                    fmt::format("RPM Spool End: {:.2f}", config.RPMSpoolEnd),
+                    fmt::format("Min boost: {:.2f}", config.MinBoost),
+                    fmt::format("Max boost: {:.2f}", config.MaxBoost),
+                    fmt::format("Spool rate: {:.5f}", config.SpoolRate),
+                    fmt::format("Unspool rate: {:.5f}", config.UnspoolRate),
+                };
 
-    menu.FloatOption("RPM Spool End", config->RPMSpoolEnd, 0.0f, 1.0f, 0.01f,
-        { "At what RPM the turbo spooling rate is maximal.",
-          "1.0 RPM is rev limit."});
+                mbCtx.OptionPlusPlus(extras);
+            }
+        }
+    });
 
-    menu.FloatOption("Min boost", config->MinBoost, -1.0f, 1.0f, 0.01f, 
-        { "What the idle boost/vacuum is." });
+    /* mainmenu -> editconfigmenu */
+    mSubmenus.emplace_back(mMenuBase, "editconfigmenu", [](NativeMenu::Menu& mbCtx, CTurboScript& context) {
+        mbCtx.Title("Config edit");
+        CConfig* config = context.ActiveConfig();
+        mbCtx.Subtitle(config ? config->Name : "None");
 
-    menu.FloatOption("Max boost", config->MaxBoost, -1.0f, 1.0f, 0.01f,
-        { "What full boost is." });
-
-    menu.FloatOption("Spool rate", config->SpoolRate, 0.01f, 0.99999f, 0.00005f,
-        { "How fast the turbo spools up, in part per 1 second.",
-          "So 0.5 is it spools up to half its max after 1 second.",
-          "0.999 is almost instant. Keep under 1.0." });
-
-    menu.FloatOption("Unspool rate", config->UnspoolRate, 0.01f, 0.99999f, 0.00005f,
-        { "How fast the turbo slows down. Calculation is same as above." });
-
-    if (menu.Option("Save changes")) {
-        config->Write();
-        UI::Notify("Saved changes", true);
-        context.LoadConfigs();
-        context.UpdateActiveConfig();
-    }
-
-    if (menu.Option("Save as...")) {
-        UI::Notify("Enter new config name." ,true);
-        std::string newName = UI::GetKeyboardResult();
-
-        UI::Notify("Enter model(s).", true);
-        std::string newModel = UI::GetKeyboardResult();
-
-        if (newName.empty() || newModel.empty()) {
-            UI::Notify("No config name or model name entered. Not saving anything.", true);
+        if (config == nullptr) {
+            mbCtx.Option("No active configuration");
             return;
         }
 
-        if (config->Write(newName, newModel))
-            UI::Notify("Saved as new configuration", true);
-        else
-            UI::Notify("Failed to save as new configuration", true);
-        context.LoadConfigs();
-        context.UpdateActiveConfig();
-    }
+        mbCtx.FloatOption("RPM Spool Start", config->RPMSpoolStart, 0.0f, 1.0f, 0.01f,
+            { "At what RPM the turbo starts spooling up.",
+              "0.2 RPM is idle." });
+
+        mbCtx.FloatOption("RPM Spool End", config->RPMSpoolEnd, 0.0f, 1.0f, 0.01f,
+            { "At what RPM the turbo spooling rate is maximal.",
+              "1.0 RPM is rev limit." });
+
+        mbCtx.FloatOption("Min boost", config->MinBoost, -1.0f, 1.0f, 0.01f,
+            { "What the idle boost/vacuum is." });
+
+        mbCtx.FloatOption("Max boost", config->MaxBoost, -1.0f, 1.0f, 0.01f,
+            { "What full boost is." });
+
+        mbCtx.FloatOption("Spool rate", config->SpoolRate, 0.01f, 0.99999f, 0.00005f,
+            { "How fast the turbo spools up, in part per 1 second.",
+              "So 0.5 is it spools up to half its max after 1 second.",
+              "0.999 is almost instant. Keep under 1.0." });
+
+        mbCtx.FloatOption("Unspool rate", config->UnspoolRate, 0.01f, 0.99999f, 0.00005f,
+            { "How fast the turbo slows down. Calculation is same as above." });
+
+        if (mbCtx.Option("Save changes")) {
+            config->Write();
+            UI::Notify("Saved changes", true);
+            context.LoadConfigs();
+            context.UpdateActiveConfig();
+        }
+
+        if (mbCtx.Option("Save as...")) {
+            UI::Notify("Enter new config name.", true);
+            std::string newName = UI::GetKeyboardResult();
+
+            UI::Notify("Enter model(s).", true);
+            std::string newModel = UI::GetKeyboardResult();
+
+            if (newName.empty() || newModel.empty()) {
+                UI::Notify("No config name or model name entered. Not saving anything.", true);
+                return;
+            }
+
+            if (config->Write(newName, newModel))
+                UI::Notify("Saved as new configuration", true);
+            else
+                UI::Notify("Failed to save as new configuration", true);
+            context.LoadConfigs();
+            context.UpdateActiveConfig();
+        }
+    });
 }
 
 void CScriptMenu::Tick(CTurboScript& turboScript) {
-    mMenu.CheckKeys();
+    mMenuBase.CheckKeys();
 
-    /* mainmenu */
-    if (mMenu.CurrentMenu("mainmenu")) { update_mainmenu(mMenu, turboScript); }
+    for (auto& submenu : mSubmenus) {
+        submenu.Update(turboScript);
+    }
 
-    /* mainmenu -> configsmenu */
-    if (mMenu.CurrentMenu("configsmenu")) { update_configsmenu(mMenu, turboScript); }
-
-    /* mainmenu -> editconfigmenu */
-    if (mMenu.CurrentMenu("editconfigmenu")) { update_editconfigmenu(mMenu, turboScript); }
-
-
-    mMenu.EndMenu();
+    mMenuBase.EndMenu();
 }
--- TurboFix/ScriptMenu.hpp
+++ TurboFix/ScriptMenu.hpp
@@ -14,7 +14,29 @@ public:
     void Tick(CTurboScript& turboScript);
 
 private:
+    class CSubmenu {
+    public:
+        CSubmenu(NativeMenu::Menu& menuCtx, std::string name, 
+            std::function<void(NativeMenu::Menu&, CTurboScript&)> menuBody)
+            : mMenuCtx(menuCtx)
+            , mName(std::move(name))
+            , mBody(std::move(menuBody)) { }
+
+        void Update(CTurboScript& context) {
+            if (mMenuCtx.CurrentMenu(mName))
+                mBody(mMenuCtx, context);
+        }
+    private:
+        NativeMenu::Menu& mMenuCtx;
+        std::string mName;
+        std::string mTitle;
+        std::string mSubtitle;
+        std::function<void(NativeMenu::Menu&, CTurboScript&)> mBody;
+        
+    };
+
     std::string mSettingsFile;
-    NativeMenu::Menu mMenu;
+    NativeMenu::Menu mMenuBase;
+    std::vector<CSubmenu> mSubmenus;
 };
 
